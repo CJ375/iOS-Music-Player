aed958d7e89501c69e1229e7b0dfdcd8cccee533
Charlie Johnson <1013545@studio2-8803.ad.ccgs.wa.edu.au>
2014-09-01 12:40:51 +0800

    Initial Commit

---
+++ SyncedMusicPlayer/SyncedMusicPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SyncedMusicPlayer
+//
+//  Created by Charlie Johnson on 9/1/14.
+//  Copyright (c) 2014 Charlie Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SyncedMusicPlayer/SyncedMusicPlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SyncedMusicPlayer
+//
+//  Created by Charlie Johnson on 9/1/14.
+//  Copyright (c) 2014 Charlie Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
